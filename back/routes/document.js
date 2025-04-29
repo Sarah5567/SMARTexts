84b0902f11e77924014e4580fcec20d93e3b73cd
sarah5567 <Sarah556726@gmail.com>
2025-04-29 23:58:14 +0300

    adding route to translate function

---
+++ back/routes/document.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router();
+const DocumentController = require('../controllers/document')
+const {verifyJWT}= require('../middlewares/verifyJWT')
+
+router.get('/getDocument', verifyJWT, DocumentController.getDocument)
+router.get('/searchDocuments', verifyJWT, DocumentController.searchDocuments)
+router.post('/updateDocument', verifyJWT, DocumentController.updateDocument)
+router.delete('/deleteDocument', verifyJWT, DocumentController.deleteDocument)
+
+router.post('/createDocument', verifyJWT, DocumentController.createDocument)
+router.post('/deepSearch', verifyJWT, DocumentController.deepSearch)
+router.post('/translate', verifyJWT, DocumentController.translate )
+
+module.exports = router
