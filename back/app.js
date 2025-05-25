6b78bec762958241c0d176a812e4976030316a96
Sarah Gershuni <Sarah556726@gmail.com>
2025-05-25 04:27:53 +0300

    adding URL to cors

--- back/app.js
+++ back/app.js
@@ -19,7 +19,8 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
-        'http://localhost:5175'
+        'http://localhost:5175',
+        'http://localhost:5176'
     ],
     credentials: true
 }));
