1d424105509335a4508d96c29eb93f88321a8a88
Sarah Gershuni <Sarah556726@gmail.com>
2025-05-26 03:42:14 +0300

    black background

--- front/src/components/Document/Modal.jsx
+++ front/src/components/Document/Modal.jsx
@@ -7,8 +7,14 @@ const Modal = ({ isOpen, onClose, title, children }) => {
     return (
         <div
             className="fixed inset-0 bg-black/65 flex items-center justify-center z-50"
-            onClick={onClose}>
-            <div className="bg-white rounded-xl shadow-2xl w-full max-w-3xl max-h-screen overflow-y-auto m-4 border-2 border-blue-200">
+            onClick={onClose}
+        >
+            <div
+                className="bg-white rounded-xl shadow-2xl w-full max-w-3xl max-h-screen overflow-y-auto m-4 border-2 border-blue-200"
+                onClick={(e)=>{
+                    e.stopPropagation()
+                }}
+            >
                 <div className="bg-gradient-to-r from-blue-50 to-blue-100 px-8 py-6 rounded-t-xl border-b border-blue-200">
                     <div className="flex justify-between items-center">
                         <h3 className="text-2xl font-semibold text-blue-900">{title}</h3>
