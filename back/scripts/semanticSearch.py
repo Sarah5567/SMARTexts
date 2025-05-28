2fa4a5f0ef82aa06044110c1c71a7ea2212c24e8
Sarah Gershuni <Sarah556726@gmail.com>
2025-05-28 16:52:19 +0300

    removing unused lines

--- back/scripts/semanticSearch.py
+++ back/scripts/semanticSearch.py
@@ -1,28 +1,3 @@
-# import cohere
-# import numpy as np
-# from sklearn.metrics.pairwise import cosine_similarity
-# import sys
-
-# def embedded(texts):
-
-#     co = cohere.ClientV2("***")
-
-#     response = co.embed(
-#         texts = texts,
-#         model="embed-english-v3.0",
-#         input_type="classification",
-#         embedding_types=["float"],
-#     )
-#     return response.embeddings.float_
-
-# texts = sys.argv[1]
-# query = sys.argv[2]
-# similarities = cosine_similarity(embedded([query]), embedded(texts))
-
-# similarity_scores = similarities[0]
-# sorted_indices = np.argsort(similarity_scores)[::-1]
-# top_documents = [texts[i] for i in sorted_indices]
-# print(json.dumps(top_documents))
 import cohere
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
@@ -75,19 +50,6 @@ def main():
         print("Usage: script.py '[\"text1\", \"text2\", ...]' \"query string\"")
         sys.exit(1)
 
-    # response = co.rerank(
-    #     model = "rerank-multilingual-v2.0",
-    #     query = query,
-    #     documents = texts,
-    #     top_n = 10
-    # )
-    # top_documents = [
-    #     {
-    #         "text": texts[result.index],
-    #         "score": result.relevance_score
-    #     }
-    #     for result in response.results
-    # ]
 
     #print(json.dumps(top_documents))
     # Compute cosine similarity between the query and all documents
